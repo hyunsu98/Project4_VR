87097126e9bf299d3e9af6af69dc4a1ac7f188fe
hyunsu98 <108320515+hyunsu98@users.noreply.github.com>
2023-09-06 20:42:13 +0900

    리플레이시작

---
+++ Assets/LHS/01_Scripts/ReplayOynatici.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//플레이어? 
+public class ReplayOynatici : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
