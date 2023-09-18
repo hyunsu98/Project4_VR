75d3169305824f4a780824c750ec00d6a186b589
parkwish <parkthwon@kakao.com>
2023-09-18 18:04:14 +0900

    Sorry

--- Assets/PSW/01_Scripts/Teleport.cs
+++ Assets/PSW/01_Scripts/Teleport.cs
@@ -1,79 +1,112 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
     public Transform hand;
-    public GameObject player;
-    LineRenderer lr;
+    public LineRenderer lr;
     public Transform marker;
-    // 마커 크기 조절하는 공식 변수
     public float kAdjust = 1;
+    public float maxLineDistance = 3f;
 
-
-    // Start is called before the first frame update
+    private bool isPlacingPlayer = false;
+    private GameObject spawnObject;
+    private Vector3 placementPosition;
+    bool isClickPending = false; // 클릭 대기 상태를 추적
     void Start()
     {
         lr = GetComponent<LineRenderer>();
         lr.enabled = false;
-        // 마커 크기를 조정하기 위해 거리를 곱한 공식
         marker.localScale = Vector3.one * kAdjust;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // 핸드의 위치에서 핸드의 앞방향으로 Ray를 발사하고
+        if (isPlacingPlayer)
+        {
+            placementPosition = hand.position + hand.forward * maxLineDistance;
+
+            if (spawnObject != null)
+            {
+                // 플레이어의 위치를 땅의 표면으로 조정
+                placementPosition.y = GetGroundHeight(placementPosition);
+                spawnObject.transform.position = placementPosition;
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                PlacePlayer();
+            }
+        }
+    }
+
+    public void OnClickSelect(string name)
+    {
         Ray ray = new Ray(hand.position, hand.forward);
         lr.SetPosition(0, ray.origin);
         RaycastHit hitInfo;
-        bool isHit = Physics.Raycast(ray, out hitInfo);
-        // 부딪힌 곳이 있다면 플레이어를 그 곳으로 배치하고 싶다.
+        bool isHit = Physics.Raycast(ray, out hitInfo, maxLineDistance);
+
+        
         if (isHit)
         {
-            // 어딘가 부딪혔다.
             lr.SetPosition(1, hitInfo.point);
             marker.position = hitInfo.point;
             marker.up = hitInfo.normal;
             marker.localScale = Vector3.one * kAdjust * hitInfo.distance;
-        }
-        else
-        {
-            lr.SetPosition(1, ray.origin + ray.direction * 100);
-            marker.position = ray.origin + ray.direction * 100;
-            marker.up = -ray.direction;
-            marker.localScale = Vector3.one * kAdjust * 100;
-        }
 
-        if (Input.GetButtonDown("Fire1"))
-        {
-            // 어딘가 부딪힘
-            lr.enabled = true;
-        }
-        else if (Input.GetButtonUp("Fire1"))
-        {
-            // 떼면 선을 안 보고 싶다.
-            lr.enabled = false;
-            // 만약 닿은 곳이 있다면?
-            if(isHit)
+            if (!isPlacingPlayer)
             {
-                // 만약 닿은 곳에 "Ground" 태그가 있다면 그 위치로 인스턴스화 하고싶다.
-                if (hitInfo.collider.CompareTag("Ground"))
+                lr.enabled = true;
+                isPlacingPlayer = true;
+                placementPosition = hand.position + hand.forward * maxLineDistance;
+                if (isHit && hitInfo.collider.CompareTag("Ground"))
                 {
-                    Instantiate(player, hitInfo.point, Quaternion.identity);
+                    GameObject tmp = Resources.Load(name) as GameObject;
+                    Vector3 spawnPosition = hitInfo.point;
+
+
+                    // 플레이어의 위치를 땅의 표면으로 조정
+                    spawnPosition.y = GetGroundHeight(spawnPosition);
+                    // spawnPosition.y = 0f; // player가 땅에 박히는 현상이 생김으로 y 값을 0으로 고정
+                    GameObject obj = Instantiate(tmp, spawnPosition, Quaternion.identity);
+               
+                    spawnObject = obj;
+
+                    isClickPending = true; // 첫 번째 클릭 이벤트가 발생한 후 두 번째 클릭 대기 상태로 변경
                     print("내려놓고싶음");
-                    //Instantiate(gameObject, hitInfo.collider.transform);
                 }
             }
         }
+        else
+        {
+            lr.enabled = false;
+            lr.SetPosition(1, ray.origin + ray.direction * maxLineDistance);
+            marker.position = ray.origin + ray.direction * maxLineDistance;
+            marker.up = -ray.direction;
+            marker.localScale = Vector3.one * kAdjust * maxLineDistance;
+        }
+    }
 
-        //if(Input.GetKeyDown(KeyCode.Alpha1))
-        //{
-        //    Instantiate(player, transform.position, Quaternion.identity);
-        //    print("내려놓고싶음");
-        //}
+    private void PlacePlayer()
+    {
+        if (spawnObject != null)
+        {
+            isClickPending = false; // 두 번째 클릭 이벤트가 처리됨
+        }
+        lr.enabled = false;
+        isPlacingPlayer = false;
+    }
+
+    private float GetGroundHeight(Vector3 position)
+    {
+        Ray ray = new Ray(position + Vector3.up * 100f, Vector3.down);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity))
+        {
+            return hitInfo.point.y;
+        }
+
+        return 0f; // 땅을 찾지 못한 경우 기본값으로 0을 반환
     }
 }
