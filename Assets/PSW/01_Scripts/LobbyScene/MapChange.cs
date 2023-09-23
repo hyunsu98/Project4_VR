70f389aa34e886a5a3148529bdb72c5480926e5b
hyunsu98 <108320515+hyunsu98@users.noreply.github.com>
2023-09-23 23:24:21 +0900

    에셋

--- Assets/PSW/01_Scripts/LobbyScene/MapChange.cs
+++ Assets/PSW/01_Scripts/LobbyScene/MapChange.cs
@@ -12,6 +12,6 @@ public class MapChange : MonoBehaviour
     public void OnClickMap()
     {
         // Scene PSW_REC2 로 변경할 것
-        SceneManager.LoadScene("PSW_REC2");
+        SceneManager.LoadScene("LHS_Main");
     }
 }
